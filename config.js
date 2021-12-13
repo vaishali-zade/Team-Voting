818e28816c154ab6b317c969710a6281da652ec6
vaishali-zade <95169009+vaishali-zade@users.noreply.github.com>
2021-12-13 18:44:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAEHY5ADsNgS0yJnAcJcbkB13888xQQwzQ",
+  authDomain: "team-voting-app-30850.firebaseapp.com",
+  databaseURL: "https://team-voting-app-30850-default-rtdb.firebaseio.com",
+  projectId: "team-voting-app-30850",
+  storageBucket: "team-voting-app-30850.appspot.com",
+  messagingSenderId: "934894638533",
+  appId: "1:934894638533:web:d19e52ecb713dbbe5527ac",
+  measurementId: "G-VNE3DZPKPJ"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
